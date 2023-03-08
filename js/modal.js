07f863b359342c5f4c9def04e29806df4a00e048
DimaHavr <109313387+DimaHavr@users.noreply.github.com>
2023-03-08 21:18:27 +0200

    update

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,34 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
-  };
+const refs = {
+  openModalBtn: document.querySelector('[data-modal-open]'),
+  closeModalBtn: document.querySelector('[data-modal-close]'),
+  modal: document.querySelector('[data-modal]'),
+  form: document.querySelector('.modal-form'),
+};
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+refs.openModalBtn.addEventListener('click', toggleModal);
+refs.closeModalBtn.addEventListener('click', toggleModal);
 
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-  }
-})();
+function toggleModal() {
+  refs.modal.classList.toggle('is-hidden');
+}
+
+refs.form.addEventListener('submit', submitForm);
+
+function submitForm(event) {
+  event.preventDefault();
+  refs.form.reset();
+  toggleModal();
+  Toastify({
+    text: 'Form submitted!',
+    duration: 2000,
+    newWindow: true,
+    close: true,
+    gravity: 'top', // `top` or `bottom`
+    position: 'center', // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+      background: '#2196f3',
+      color: '#fff',
+    },
+  }).showToast();
+}
