58b9ee90e32caf563c8141442303bbd6fa7e4cfb
DimaHavr <109313387+DimaHavr@users.noreply.github.com>
2023-03-17 23:54:03 +0200

    update

--- js/modal.js
+++ js/modal.js
@@ -3,6 +3,7 @@ const refs = {
   closeModalBtn: document.querySelector('[data-modal-close]'),
   modal: document.querySelector('[data-modal]'),
   form: document.querySelector('.modal-form'),
+  footerForm: document.querySelector('.footer-form'),
 };
 
 refs.openModalBtn.addEventListener('click', toggleModal);
@@ -10,6 +11,7 @@ refs.closeModalBtn.addEventListener('click', toggleModal);
 
 function toggleModal() {
   refs.modal.classList.toggle('is-hidden');
+  document.body.classList.toggle('is-fixed');
 }
 
 refs.form.addEventListener('submit', submitForm);
@@ -20,6 +22,25 @@ function submitForm(event) {
   toggleModal();
   Toastify({
     text: 'Form submitted!',
+    duration: 2000,
+    newWindow: true,
+    close: true,
+    gravity: 'top', // `top` or `bottom`
+    position: 'center', // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+      background: '#2196f3',
+      color: '#fff',
+    },
+  }).showToast();
+}
+
+function submitForm(event) {
+  event.preventDefault();
+  refs.form.reset();
+  toggleModal();
+  Toastify({
+    text: 'Заявка надіслана!',
     duration: 2000,
     newWindow: true,
     close: true,
